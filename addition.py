b66e2f00da4359d7ad43fd454353dabc7bf8f6f8
Teddy-Sannan <54954634+Teddy-Sannan@users.noreply.github.com>
2019-10-31 13:01:45 -0400

    Update addition.py

--- addition.py
+++ addition.py
@@ -2,17 +2,23 @@
 
 # Created by: Teddy Sannan
 # Created on: October 2019
-# This lets the user guess a number
+# This adds user numbers except negatives
 
 
 def main():
+    # this adds user numbers except negatives
+    # helps revents crashing
     while True:
+        # prgrams main function
         try:
+            # list of all positive numbers
             total_numbers = []
 
+            # input
             number = int(input("How many numbers are you going to add: "))
             print()
 
+            # process
             for addition in range(number):
                 numbers = int(input("Enter a number to add: "))
 
@@ -21,15 +27,18 @@ def main():
 
                 total_numbers.append(numbers)
 
+            # output
             print()
             print("Sum of just the positive numbers is =", sum(total_numbers))
 
+        # prevents false user inputs
         except ValueError:
             print()
             print("Invalid Input")
             print()
             continue
 
+        # break out of loop
         else:
             break
 
